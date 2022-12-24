fed0a5088ccd219335d62b00dfc29b3f37875294
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2022-12-24 16:52:05 +0900

    トップの画像、レスポンシブ対応

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,23 @@
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+hamburgerBtn.addEventListener("click", (e) => {
+    document.querySelector("header nav ul").classList.toggle("active");
+});
+
+
+let popUpTargets = document.querySelectorAll('.scrollPopUp,.scrollLeftPop,.scrollRightPop');
+
+window.addEventListener('scroll', function () {
+    var scroll = window.scrollY; //スクロール量を取得
+    var windowHeight = window.innerHeight; //画面の高さを取得
+    for (let target of popUpTargets) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+        var targetPos = target.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+        if (scroll > targetPos - windowHeight) { //スクロール量 > ターゲット要素の位置
+            target.classList.add('blockIn');
+        }
+    }
+});
+
+
 // const internalATag = document.querySelectorAll('a[href^="#"]');
 // internalATag.forEach(aTag => aTag.addEventListener("click", (e) => {
 //     e.target.getAttribute('href');
